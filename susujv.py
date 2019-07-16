51f6afbb503c9dd5f6ded1c2f9996ab19ae71fc8
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-16 02:46:44 -0700

    Create susujv.py

---
+++ susujv.py
@@ -0,0 +1,6 @@
+for i in range(99,0,-1):
+    print i, "Bottles of beer on the wall,"
+    print i, "bottles of beer."
+    print "Take one down and pass it around,"
+    print i-1, "bottles of beer on the wall."
+    print
